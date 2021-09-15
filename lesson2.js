19aa4418be81bd691bdb299f67004605b37d2d9d
mathgurgel <88843455+mathgurgel@users.noreply.github.com>
2021-09-14 21:35:52 -0300

    Fix Output
    Transform output into scientific notation.

--- lesson2.js
+++ lesson2.js
@@ -2,10 +2,12 @@ function Convert()
 {
     let elementValue = document.getElementById("valueKm");
     let value = elementValue.value;
-    let valueKm = parseFloat(value);
 
-    let valueLy = (valueKm * 0.00000000000010570).toFixed(4);
+    let valueKm = parseFloat(value);
+    if(valueKm >= 99999.99) valueKm = valueKm.toExponential(); // 99999.99: float imprecision
+
+    let valueLy = (valueKm * 1.057e-13).toExponential(3);
     let convertedValueElement = document.getElementById("convertedValue");
-    let convertedValue = valueKm + " equals to " + valueLy + " light-years.";
+    let convertedValue = valueKm + "km equals to " + valueLy + " light-years.";
     convertedValueElement.innerHTML = convertedValue;
 }
